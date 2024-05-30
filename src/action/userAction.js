0c28590feb101fc88424f11b8e2a9b7aae8d6e85
jin <jin200126@gmail.com>
2024-05-30 23:47:33 +0900

    register

--- src/action/userAction.js
+++ src/action/userAction.js
@@ -2,15 +2,31 @@ import api from "../utils/api";
 import * as types from "../constants/user.constants";
 import { commonUiActions } from "./commonUiAction";
 import * as commonTypes from "../constants/commonUI.constants";
-const loginWithToken = () => async (dispatch) => {};
-const loginWithEmail = (payload) => async (dispatch) => {};
-const logout = () => async (dispatch) => {};
+import { type } from "@testing-library/user-event/dist/type";
+const loginWithToken = () => async (dispatch) => { };
+const loginWithEmail = (payload) => async (dispatch) => { };
+const logout = () => async (dispatch) => { };
 
-const loginWithGoogle = (token) => async (dispatch) => {};
+const loginWithGoogle = (token) => async (dispatch) => { };
 
-const registerUser =
-  ({ email, name, password }, navigate) =>
-  async (dispatch) => {};
+const registerUser = ({ email, name, password }, navigate) =>
+  async (dispatch) => {
+    try {
+      dispatch({ type: types.REGISTER_USER_REQUEST })
+      const response = await api.post("/user", { email, name, password })
+      if (response.status !== 200) {
+        throw new Error(response.error)
+      }
+      dispatch(commonUiActions.showToastMessage("회원가입을 완료했습니다.", "success"))
+      navigate('/login')
+      dispatch({ type: types.REGISTER_USER_SUCCESS })
+    } catch (error) {
+      dispatch({ type: types.REGISTER_USER_FAIL, payload: error.error })
+    }
+  };
+
+
+
 export const userActions = {
   loginWithToken,
   loginWithEmail,
--- src/page/RegisterPage.js
+++ src/page/RegisterPage.js
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Form, Button, Alert } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router";
 import { userActions } from "../action/userAction";
 import "../style/register.style.css";
+
 const RegisterPage = () => {
   const dispatch = useDispatch();
+
   const [formData, setFormData] = useState({
     email: "",
     name: "",
@@ -13,6 +15,7 @@ const RegisterPage = () => {
     confirmPassword: "",
     policy: false,
   });
+
   const navigate = useNavigate();
   const [passwordError, setPasswordError] = useState("");
   const [policyError, setPolicyError] = useState(false);
@@ -20,15 +23,35 @@ const RegisterPage = () => {
 
   const register = (event) => {
     event.preventDefault();
+    const { email, name, password, confirmPassword, policy } = formData
     // 비번 중복확인 일치하는지 확인
+    if (password !== confirmPassword) {
+      setPasswordError("비밀번호와 재입력 비밀번호가 일치하지 않습니다.")
+      return;
+    }
     // 이용약관에 체크했는지 확인
+    if (!policy) {
+      setPolicyError(true)
+      return;
+    }
     // FormData에 있는 값을 가지고 백엔드로 넘겨주기
+    setPasswordError("")
+    setPolicyError(false)
+    dispatch(userActions.registerUser({ email, name, password }, navigate))
     //성공후 로그인 페이지로 넘어가기
   };
 
   const handleChange = (event) => {
     event.preventDefault();
-    // 값을 읽어서 FormData에 넣어주기
+    const { id, value, checked } = event.target
+
+    console.log("id:", id, checked)
+    if (id === 'policy') {
+      setFormData({ ...formData, [id]: checked })
+
+    } else {
+      setFormData({ ...formData, [id]: value })
+    }
   };
 
   return (
@@ -83,6 +106,7 @@ const RegisterPage = () => {
           />
           <Form.Control.Feedback type="invalid">
             {passwordError}
+            {/* {passwordError ? passwordError : policyError ? policyError : ""} */}
           </Form.Control.Feedback>
         </Form.Group>
         <Form.Group className="mb-3">
