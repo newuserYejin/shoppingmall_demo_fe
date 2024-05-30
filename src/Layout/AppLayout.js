22e57bbce5531857dd2e926223d6ab226e73981c
jin <jin200126@gmail.com>
2024-05-31 00:56:45 +0900

    update

--- src/Layout/AppLayout.js
+++ src/Layout/AppLayout.js
@@ -11,8 +11,8 @@ import { commonUiActions } from "../action/commonUiAction";
 const AppLayout = ({ children }) => {
   const location = useLocation();
   const dispatch = useDispatch();
-  // const user = null;
-  const user = { level: "admin" }; // 로그인 기능 만들고 지우기
+  const user = null;
+  // const user = { level: "admin" }; // 로그인 기능 만들고 지우기
 
   // const { user } = useSelector((state) => state.user);
   useEffect(() => {
