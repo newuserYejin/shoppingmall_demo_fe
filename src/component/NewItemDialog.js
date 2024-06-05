d59436b28bc9b5b2ca35c25b6854f3680e25b5be
jin <jin200126@gmail.com>
2024-06-06 02:03:36 +0900

    admin page product add

--- src/component/NewItemDialog.js
+++ src/component/NewItemDialog.js
@@ -33,6 +33,8 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
   const handleClose = () => {
     //모든걸 초기화시키고;
     // 다이얼로그 닫아주기
+    setFormData(InitialFormData)
+    setShowDialog(false)
   };
 
   const handleSubmit = (event) => {
